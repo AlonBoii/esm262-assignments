5b4ade76a8a2ad95673922abd6e7bad8fdd59c39
AlonBoii <alon.robbins@gmail.com>
2025-02-25 08:48:06 -0800

    made changes Writing_Functions_Alon_Robbins.R where I fixed spacing errors

--- R/Writing_Functions_Alon_Robbins.R
+++ R/Writing_Functions_Alon_Robbins.R
@@ -15,7 +15,7 @@
 
 log_rate <- function(N, r, K) {
   
-  # error checking
+# error checking
 population = ifelse(N < 0, return("N must be greater than or equal to 0"), N) # population size
   
 growth_rate = ifelse(r < 0, return("r must be greater than or equal to 0"), r) # growth rate
@@ -23,8 +23,6 @@ growth_rate = ifelse(r < 0, return("r must be greater than or equal to 0"), r) #
 carrying_cap = ifelse(K < 0, return("K must be greater than or equal to 0"), K) # carrying capacity
   
 
-  
-  
 # calculate the rate of change in population size over time
 dN_dt <- r*N*(1-N/K)
   
